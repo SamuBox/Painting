e33bd867d49fd6dd3408739518c4f6a2c3bf2cd6
Samuel <samuel.lopez.prieto@gmail.com>
2018-08-31 20:04:02 +0200

    fixed moving into new dimensions, added enum for directions

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
-//veticalmente tambien pasa, hay que dar opcion de rellenar los cuadros vertical u horizontal
-
-var cols=5;
-var rows=4;
+//TODO fix jumping into a new dimension
+//TODO veticalmente tambien pasa, hay que dar opcion de rellenar los cuadros vertical u horizontal
+//TODO check that at least one of rows and cols is an even number so every square has a possible pair
+var cols=4;
+var rows=5;
 //original painting: 4,5
 var siz=50; // size of the squares
 var mov_size = 2; //how many squares to jump
 var content = [];
+
+const directions = {
+    UP: 'up',
+    DOWN: 'down',
+    LEFT: 'left',
+    RIGHT: 'right'
+}
 
 function make2Darray(cols,rows){
 	var arr = new Array(cols);
@@ -20,6 +28,7 @@ function setup() {
 	content = make2Darray(cols,rows);
 	var N = cols*rows/2;
 	var cont =0;
+  //TODO filling the content horizontally, add option to fill vertically
 	for(var i = 0;i<rows;i++){
 		for(var j = 0;j<cols;j++){
 			//content[i][j] = random(255);
@@ -35,39 +44,76 @@ function check(oriCont, newCont){
 	return oriCont==newCont;
 }
 
-function movs(i,j,content){
-	//up
-	var newJ =j, newI=i;
-	if(j>mov_size-1)
-		newJ=j-2;
-	else {
-		newJ = (rows-1)-(mov_size-1-j);
+function getNewRow(j, dir){
+	var newRow;
+	if(dir==directions.UP){
+		if(j>mov_size-1) //go straight up
+			newRow=j-mov_size;
+		else { //go down
+			newRow = (rows-1)-(mov_size-1-j);
+		}
 	}
+	else if(dir==directions.DOWN){
+		if(j+mov_size<=rows-1) //go straight down
+			newRow=j+mov_size;
+		else { // go up
+			newRow = 0 +  mov_size - 1 - ((rows-1)-j);
+		}
+	}
+	//else{} TODO throw exceptcion
+	return newRow;
+
+}
+
+function getNewCol(i, dir){
+	if(dir==directions.RIGHT){
+		var newCol;
+		if(i+mov_size<=cols-1) //go straight right
+			newCol=i+mov_size;
+		else { //go into the left part
+			newCol = 0 +  mov_size - 1 - ((cols-1)-i);
+		}
+
+	}
+	if(dir==directions.LEFT){
+		var newCol;
+		if(i>mov_size-1) //go straight right
+			newCol=i-mov_size;
+		else { //go into the left part
+			newCol = (cols-1)-(mov_size-1-i);
+		}
+
+	}
+
+	return newCol;
+
+}
+
+function movs(i,j,content){
+	var newJ , newI;
+
+	newJ = getNewRow(j,directions.UP);
+	newI = getNewCol(i,directions.LEFT);
+	//fill gives a color to everything under it, until the next fill
 	if (check(content[i][j],content[newI][newJ]))
 		fill(255,0,0);
 	else
 		fill(255,255,255);
 
+
 }
-//TODO: rows and columns are inverted, fix before continuing PLZ
+
 function draw() {
 	background(255);
 	for(var i = 0;i<cols;i++){
 		for(var j = 0;j<rows;j++){
 			var x = i * siz;
 			var y = j * siz;
-			//fill gives a color to everything under it, or until the next fill
-			//color a rectangle
-			// if(i==0)
-			// 	fill(255,0,0);
-			// else
-			// 	fill(255,255,255);
 			movs(i,j,content);
 			rect(x,y,siz,siz);
 			//black for numbers
 			fill(0,0,0);
 			text(content[i][j],x+siz/2 ,y + siz/2);
 		}
-	}//terminamos de pintar
-
+	}
 }
