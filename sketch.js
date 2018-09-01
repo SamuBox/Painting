e0e7fb9bc0129990ae50480b1fcedb152272695e
Samuel <samuel.lopez.prieto@gmail.com>
2018-09-01 12:40:00 +0200

    pairs have the same color now, a random one instead of red

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-//TODO fix jumping into a new dimension
 //TODO veticalmente tambien pasa, hay que dar opcion de rellenar los cuadros vertical u horizontal
 //TODO check that at least one of rows and cols is an even number so every square has a possible pair
 var cols=4;
@@ -38,6 +37,52 @@ function setup() {
 			cont++;
 		}
 	}
+  drawPainting();
+}
+
+function drawPainting(){
+  var N= cols*rows/2;
+  var cont = 0;
+  for(var i = 0;i<cols && cont<N;i++){
+    for(var j = 0;j<rows && cont<N;j++){
+      var RGB = getRandomColor();
+      // var x = i * siz;
+      // var y = j * siz;
+      movs(i,j,content,RGB);
+      fill(RGB[0],RGB[1],RGB[2]); //original rect will always have a color
+      drawRect(i,j);
+      // rect(x,y,siz,siz);
+      // //black for numbers
+      // fill(0,0,0);
+      // text(content[i][j],x+siz/2 ,y + siz/2);
+      cont++;
+    }
+  }
+}
+
+function drawRect(i,j){
+  var x = i * siz;
+  var y = j * siz;
+  rect(x,y,siz,siz);
+  //black for numbers
+  fill(0,0,0);
+  text(content[i][j],x+siz/2 ,y + siz/2);
+}
+
+function movs(i,j,content,RGB){
+	var newJ , newI;
+
+	newJ = getNewRow(j,directions.DOWN);
+	newI = getNewCol(i,directions.RIGHT);
+	//fill gives a color to everything under it, until the next fill
+	if (check(content[i][j],content[newI][newJ])){
+    fill(RGB[0],RGB[1],RGB[2]);
+    drawRect(newI,newJ);
+  }
+	else{
+    fill(255,255,255);
+  }
+
 }
 
 function check(oriCont, newCont){
@@ -82,38 +127,21 @@ function getNewCol(i, dir){
 		else { //go into the left part
 			newCol = (cols-1)-(mov_size-1-i);
 		}
-
 	}
-
 	return newCol;
-
 }
 
-function movs(i,j,content){
-	var newJ , newI;
 
-	newJ = getNewRow(j,directions.UP);
-	newI = getNewCol(i,directions.LEFT);
-	//fill gives a color to everything under it, until the next fill
-	if (check(content[i][j],content[newI][newJ]))
-		fill(255,0,0);
-	else
-		fill(255,255,255);
-
-
+//return a random size 3 RGB array
+function getRandomColor(){
+  var RGB = new Array(3);
+  for(var i=0;i<3;i++)
+    RGB[i] =  Math.floor((Math.random() * 255));
+  return RGB;
 }
 
 function draw() {
-	background(255);
-	for(var i = 0;i<cols;i++){
-		for(var j = 0;j<rows;j++){
-			var x = i * siz;
-			var y = j * siz;
-			movs(i,j,content);
-			rect(x,y,siz,siz);
-			//black for numbers
-			fill(0,0,0);
-			text(content[i][j],x+siz/2 ,y + siz/2);
-		}
-	}
+	//background(255);
+  //drawPainting();
+
 }
